2e33d952f2846286ae62fa248c713f8f7fc8a8b5
Marat Zaynullin <8444691@gmail.com>
2019-05-05 21:54:11 +0300

    make it better3

--- public/bundle.js
+++ public/bundle.js
@@ -17699,7 +17699,7 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   API_PREFIX: "https://todo-project-marat.herokuapp.com",
   IMAGE_PREFIX: "https://todo-project-marat.herokuapp.com/Uploads/Images",
-  MONGO_URL: "mongodb://<marat>:<5024223Qq>@ds113700.mlab.com:13700/todo-marat",
+  MONGO_URL: "mongodb://marat:5024223Qq@ds113700.mlab.com:13700/todo-marat",
   SECRET_SESSION: "secret-super-secret",
   JWT_SECRET: "welcome, man"
 };
