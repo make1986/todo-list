70c3b36361acd6c88a1a45ff820932b669022657
Marat Zaynullin <8444691@gmail.com>
2019-05-05 21:47:22 +0300

    make it better1

--- src/etc/config.js
+++ src/etc/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
-  API_PREFIX: "http://localhost:3000",
-  IMAGE_PREFIX: "http://localhost:3000/Uploads/Images",
-  MONGO_URL: "mongodb://localhost/todolist",
+  API_PREFIX: "https://todo-project-marat.herokuapp.com/",
+  IMAGE_PREFIX: "https://todo-project-marat.herokuapp.com/Uploads/Images",
+  MONGO_URL: "mongodb://<marat>:<5024223Qq>@ds113700.mlab.com:13700/todo-marat",
   SECRET_SESSION: "secret-super-secret",
   JWT_SECRET: "welcome, man"
 };
