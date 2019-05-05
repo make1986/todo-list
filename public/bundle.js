908027bf09ea7bbc9644e72e98465a4e1c088237
Marat Zaynullin <8444691@gmail.com>
2019-05-05 21:51:04 +0300

    make it better2

--- public/bundle.js
+++ public/bundle.js
@@ -17697,7 +17697,7 @@ if (!self.fetch) {
 
 module.exports = {
   PORT: process.env.PORT || 3000,
-  API_PREFIX: "https://todo-project-marat.herokuapp.com/",
+  API_PREFIX: "https://todo-project-marat.herokuapp.com",
   IMAGE_PREFIX: "https://todo-project-marat.herokuapp.com/Uploads/Images",
   MONGO_URL: "mongodb://<marat>:<5024223Qq>@ds113700.mlab.com:13700/todo-marat",
   SECRET_SESSION: "secret-super-secret",
--- server.js
+++ server.js
@@ -84,7 +84,7 @@ module.exports = require("express");
 
 module.exports = {
   PORT: process.env.PORT || 3000,
-  API_PREFIX: "https://todo-project-marat.herokuapp.com/",
+  API_PREFIX: "https://todo-project-marat.herokuapp.com",
   IMAGE_PREFIX: "https://todo-project-marat.herokuapp.com/Uploads/Images",
   MONGO_URL: "mongodb://<marat>:<5024223Qq>@ds113700.mlab.com:13700/todo-marat",
   SECRET_SESSION: "secret-super-secret",
